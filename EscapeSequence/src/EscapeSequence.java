cc48db524ec017799f26afa1bf5f0ce30bcdea5e
student <avrildouglas>
2017-05-25 17:31:24 -0400

    Add all the product text files

---
+++ EscapeSequence/src/EscapeSequence.java
@@ -0,0 +1,12 @@
+public class EscapeSequence {
+	
+	public static void main (int amount) {   
+        System.out.print("*/t*");
+        System.out.print("*/t*");
+        System.out.print("*       *");
+        System.out.print("*       *");
+        System.out.print("*       *");
+        System.out.print("*********");
+        System.out.print("*********");
+}
+}
